d5237b68b53d4fae94973bd4e941e036a64ea1fe
Omid SoheilNia <soheilniao@gmail.com>
2022-09-06 12:20:56 +0430

    categorie tiles styled

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="auto" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
--- components/CategoryGrideTile.js
+++ components/CategoryGrideTile.js
@@ -1,11 +1,17 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 
 function CategoryGrideTile({ title, color }) {
   return (
-    <View style={{ backgroundColor: color }}>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +19,33 @@ function CategoryGrideTile({ title, color }) {
 }
 
 export default CategoryGrideTile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 150,
+    margin: 16,
+    borderRadius: 8,
+    elevation: 4,
+    // IOS shadow
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: { flex: 1 },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
